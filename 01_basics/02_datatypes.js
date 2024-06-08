aaff4b86c4316d5c4855a0fcdf345576046c2242
satyajit1106 <96108441+satyajit1106@users.noreply.github.com>
2024-06-08 15:29:03 +0000

    datatype and conversion of the variables

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,14 @@
 "use strict" // treat all the JS Code as the newer version 
+let age=12
+let username="satyajit"
+let mob=626348900999999999999
+let isbig=true
+let user=undefined
+let err=null
+
+console.log(typeof(age))
+console.log(typeof(username))
+console.log(typeof(mob))
+console.log(typeof(isbig))
+console.log(typeof(user))
+console.log(typeof(err))
---
+++ 01_basics/03_Conversion_operation.js
@@ -0,0 +1,7 @@
+// let score=0
+// let convertedScore=Boolean(score)
+// console.log(convertedScore);
+
+let assume="33abc"
+let convertAssume=Number(assume)
+console.log(convertAssume);

02771a0abc04474996c942c89c3fdf1e1a9351f0
satyajit1106 <96108441+satyajit1106@users.noreply.github.com>
2024-06-12 18:52:44 +0000

    from , isArray, of in arrays

--- 02_baiscs/02_array.js
+++ 02_baiscs/02_array.js
@@ -8,24 +8,21 @@ const dc_heros = ["superman", "flash", "batman"]
 
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
+// concatination allows to append the element in the end of the array without the square brackets for another array
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
+// const all_new_heros = [...marvel_heros,...dc_heros]
 // console.log(all_new_heros);
+// ... is called the SPREAD operator that allows you to spread the elements in the array like concatination do
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const arr1=[1,2,[3,4,[5,6,10],22,45],12,56]
+// const arr2=arr1.flat(Infinity)
+// console.log(arr2)
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+console.log(Array.isArray("satyajit")); // Array.isArray() checks if the passed value is an Array. It does not check the value's prototype chain, nor does it rely on the Array constructor it is attached to.
 
+console.log(Array.from("satyajit")); // To convert an ordinary object that's not iterable or array-like to an array
 
-
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
-
-let score1 = 100
-let score2 = 200
-let score3 = 300
-
-console.log(Array.of(score1, score2, score3));
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3)); // it helps to create the list of the elements in the 

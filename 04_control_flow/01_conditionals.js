ff99e7249da92b299eedfeb2d1244aea01eb1276
satyajit1106 <96108441+satyajit1106@users.noreply.github.com>
2024-06-18 13:59:49 +0000

    edits

--- 04_control_flow/01_conditionals.js
+++ 04_control_flow/01_conditionals.js
@@ -23,7 +23,7 @@ const temperature = 41
 
 // const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2"); very unreadable code
 
 // if (balance < 500) {
 //     console.log("less than 500");

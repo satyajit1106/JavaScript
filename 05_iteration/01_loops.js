48fe92590fe28b7d8a55b5015dd3fc9b5a4fad2c
satyajit1106 <96108441+satyajit1106@users.noreply.github.com>
2024-06-18 14:24:56 +0000

    for loops basics

---
+++ 05_iteration/01_loops.js
@@ -0,0 +1,32 @@
+// for loop
+for(let  index =0; index <11; index ++){
+    console.log( index );
+}
+
+
+let myArray=["sachin","satyajit","parth"];
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+// break and continue
+
+// for (let i = 0; i < 20; i++) {
+//     if(i==5){
+//         console.log(`detected 5`);
+//         break
+//     }
+//     console.log(`value of i = ${i}`);
+    
+// }
+
+
+// for (let i = 0; i < 20; i++) {
+//     if(i==5){
+//         console.log(`detected 5`);
+//         continue
+//     }
+//     console.log(`value of i = ${i}`);
+    
+// }
+

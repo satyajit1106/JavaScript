12d8454602a4547644a5c565f69558e411ff359c
satyajit1106 <96108441+satyajit1106@users.noreply.github.com>
2024-06-18 14:40:31 +0000

    while and doWhile loops

---
+++ 05_iteration/02_while_loop.js
@@ -0,0 +1,15 @@
+// while loop
+
+let index=0
+while (index != 10) {
+    // console.log(`value of i = ${index}`);
+    index++
+}
+
+// do-while loop
+let score=11
+do {
+    console.log(`score is ${score}`);
+    score++
+
+} while (score<=10);
